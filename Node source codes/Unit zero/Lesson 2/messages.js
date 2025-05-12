c6c43d3341315dea5edb5d0944afd59391fcc988
Samuel Muvhango <93419130+muvhangos@users.noreply.github.com>
2025-05-12 13:05:27 +0200

    Add files via upload

---
+++ Node source codes/Unit zero/Lesson 2/messages.js
@@ -0,0 +1,10 @@
+let messages = [
+    "A change of environment can be a good thing!",
+    "You will make it!",
+    "Just run with the code!"
+];
+
+// Loop through the messages and log them to the console
+messages.forEach(message => {
+    console.log(message);
+});
