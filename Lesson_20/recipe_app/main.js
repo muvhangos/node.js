ece305072d2b2dd6b6ac163cd55e1c15f5964282
Samuel Muvhango <93419130+muvhangos@users.noreply.github.com>
2025-10-03 21:11:25 +0200

    Unit 4 Unit 5

---
+++ Lesson_20/recipe_app/main.js
@@ -0,0 +1,80 @@
+"use strict";
+// Imports
+const express = require("express");
+const app = express();
+const router = express.Router();
+const layouts = require("express-ejs-layouts");
+const mongoose = require("mongoose");
+
+const errorController = require("./controllers/errorController");
+const homeController = require("./controllers/homeController");
+const subscriberController = require("./controllers/subscribersController");
+const usersController = require("./controllers/usersController");
+const coursesController = require("./controllers/coursesController");
+const Subscriber = require("./models/subscriber");
+const methodOverride = require("method-override");
+
+mongoose.Promise = global.Promise;
+
+// Database Connection
+mongoose.connect("mongodb://0.0.0.0:27017/recipe_db", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+
+db.once("open", () => {
+  console.log("✅ Successfully connected to MongoDB using Mongoose!");
+});
+
+//App Configuration
+app.set("port", process.env.PORT || 3000);
+app.set("view engine", "ejs");
+
+
+// Middleware
+router.use(express.static("public"));
+router.use(layouts);
+router.use(express.urlencoded({ extended: false }));
+router.use(express.json());
+router.use(methodOverride("_method", {
+  methods: ["POST", "GET"]
+}));
+router.use(homeController.logRequestPaths);
+
+//Home Routes
+router.get("/", homeController.index);
+router.get("/contact", subscriberController.getSubscriptionPage);
+
+//User Routes
+router.get("/users", usersController.index, usersController.indexView);
+router.get("/users/new", usersController.new);
+router.post("/users/create", usersController.create, usersController.redirectView);
+router.get("/users/:id", usersController.show, usersController.showView);
+router.get("/users/:id/edit", usersController.edit);
+router.put("/users/:id/update", usersController.update, usersController.redirectView);
+router.delete("/users/:id/delete", usersController.delete, usersController.redirectView);
+
+
+//Subscriber Routes
+router.get("/subscribers", subscriberController.getAllSubscribers, (req, res, next) => {
+  res.render("subscribers", { subscribers: req.data });
+});
+
+router.post("/subscribe", subscriberController.saveSubscriber);
+
+//Course Route
+router.get("/courses", homeController.showCourses);
+
+//Error Handling
+app.use("/", router);
+router.use(errorController.logErrors);
+router.use(errorController.respondNoResourceFound);
+router.use(errorController.respondInternalError);
+
+
+//Server Start
+app.listen(app.get("port"), () => {
+  console.log(`🚀 Server running at http://localhost:${app.get("port")}`);
+});
